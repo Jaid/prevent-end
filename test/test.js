3fa95ded5dbd2eaea2d290fdf9162f1a7510c5fb
Jaid <jaid.jsx@gmail.com>
2019-02-16 21:08:13 +0100

    Fixed typo in tests
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -3,12 +3,12 @@ import path from "path"
 const indexModule = (process.env.MAIN ? path.resolve(__dirname, "..", process.env.MAIN) : path.join(__dirname, "..", "src"))
 const {default: preventEnd} = require(indexModule)
 
-it("should run run for string", () => {
+it("should run for string", () => {
   const result = preventEnd("abcd", "cd")
   expect(result).toEqual("ab")
 })
 
-it("should run run for arrays", () => {
+it("should run for arrays", () => {
   const result = preventEnd(["a", "b", "c", "d"], ["c", "d"])
   expect(result).toEqual(["a", "b"])
 })
